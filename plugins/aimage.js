e618ddfaf2cfe69d3c7ecc2cdbe7d4699da3d5e2
prasbhara0604 <prasbhara0604@gmail.com>
2024-08-17 10:04:24 +0000

    tambah dalle

---
+++ plugins/aimage.js
@@ -0,0 +1,13 @@
+import { dalle3 } from "../ai.js"
+export default async ({ sock, m, id, psn, sender, noTel, caption }) => {
+    if(psn ==""){
+        await sock.sendMessage(id, { text: "Kasih querynya dong banh,\ncontoh : *aimage loli kawaii*" })
+        return
+    }
+    try {
+        await sock.sendMessage(id, { text: 'Processing, please wait...' })
+        await sock.sendMessage(id, { image: { url: await dalle3(psn) } })
+    } catch (error) {
+        sock.sendMessage(id, { text: error })
+    }
+};
